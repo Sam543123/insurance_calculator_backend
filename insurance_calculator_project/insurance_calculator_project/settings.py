1f7ce6fbf34cf1a312453eead364c867c8a293c8
Semyon Sazanchuk <semyon.chuk@gmail.com>
2024-07-17 21:00:07 +0300

    Set up CORS.

--- insurance_calculator_project/insurance_calculator_project/settings.py
+++ insurance_calculator_project/insurance_calculator_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'insurance_calculator_app'
 ]
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'insurance_calculator_project.urls'
@@ -123,3 +125,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Allow requests to backend from this urls
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+)
